73376d137ea149ec6fc5d700f6afc065997f36f2
Felix Vargas Jr <109393467+NeverGiveUp23@users.noreply.github.com>
2022-09-22 13:10:46 -0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,21 @@
+# Felix Vargas Jr
+# I love to code!
+
+name = """
+FFFFFFFFFF
+FFF
+FFFFFFF
+FFF
+FFF
+FFF
+"""
+last_name = """
+VVV          VVV
+ VVV        VVV
+  VVV      VVV
+   VVV    VVV
+    VVV  VVV
+      VVV 
+         
+"""
+print(name, last_name)
